8d4b491a9dd185d1ff44ed39708ab70b0fa6b3e8
Mike Arango <maicol@Macbooks-MacBook-Pro.local>
2023-01-11 23:07:42 -0500

    Kata 9 check

--- src/main/java/katas/Kata9.java
+++ src/main/java/katas/Kata9.java
@@ -3,6 +3,7 @@ package katas;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import model.Bookmark;
+import model.BoxArt;
 import model.Movie;
 import model.MovieList;
 import util.DataUtil;
@@ -10,6 +11,7 @@ import util.DataUtil;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve each video's id, title, middle interesting moment time, and smallest box art url
@@ -20,6 +22,22 @@ public class Kata9 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "some title", "time", new Date(), "url", "someUrl"));
+        List<Map> movieLists2 = movieLists.stream()
+                .flatMap(movieList -> movieList.getVideos().stream())
+                .map(movie -> ImmutableMap.of("id", movie.getId(), "title", movie.getTitle()
+                                ,"time", movie.getInterestingMoments().stream()
+                                        .filter(im -> im.getType().equals("Middle"))
+                                        .map(r -> r.getTime())
+                                        .collect(Collectors.toUnmodifiableList())
+                                , "url", movie.getBoxarts().stream()
+                                        .reduce((a, b) -> {
+                                            BoxArt res = ((a.getHeight() * a.getWidth()) < (b.getHeight() * b.getWidth())) ? a : b;
+                                            return res;
+                                        })
+                                        .get()
+                                        .getUrl()))
+                        .collect(Collectors.toList());
+
+        return movieLists2;
     }
 }
